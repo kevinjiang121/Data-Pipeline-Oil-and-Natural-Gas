32078002a362aae51713f9500df206ed03a39a05
Kevin Jiang <kevinjiang121@gmail.com>
2022-12-22 18:45:06 -0500

    Moved most code regarding UI to UIWindow

--- main.py
+++ main.py
@@ -1,47 +1,11 @@
 import sys
-from WebScrape import WebScrape as Ws
-from LoadDatabase import LoadDatabase as ld
-from UIWindow import UIWindow as ui
-import pygame as pg
-
-import os
-
+from UIWindow import UIWindow
+from PyQt6 import QtWidgets, uic
+app = QtWidgets.QApplication(sys.argv)
 
 def main():
-    pg.init()
-    ui.render()
-    pg.display.update()
-    position = [0, 0]
-    path = ''
-
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.MOUSEBUTTONDOWN:
-                position = pg.mouse.get_pos()
-
-            if event.type == pg.MOUSEBUTTONUP:
-                if 250 < position[0] < 550 and 50 < position[1] < 200:
-                    path = input("Enter CSV path location: ")
-                    path = r'' + path
-                    if not os.path.exists(path):
-                        os.makedirs(path)
-
-                    dates = Ws.date_list(12, 2016, 1, 2019)
-
-                    # puts all data into a dataframe then converts it to a CSV
-                    df_all_data = Ws.scrape_site(dates, path)
-                    df_all_data.to_csv(path + '\\df_all_data.csv')
-
-                if 250 < position[0] < 550 and 300 < position[1] < 550:
-                    print('Transform')
-
-                if 250 < position[0] < 550 and 550 < position[1] < 700:
-                    ld.load_database(path)
-
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit(0)
-
+    window = UIWindow()
+    app.exec()
 
 if __name__ == "__main__":
     main()
